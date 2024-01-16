57d27ba8a4f8aee8cc302653b06311974b617964
b3east1 <rhylendelarosa@gmail.com>
2024-01-16 12:03:04 -0800

    finished lesson 2

--- 3D Beginner/Assets/UnityTechnologies/Scripts/PlayerMovement.cs
+++ 3D Beginner/Assets/UnityTechnologies/Scripts/PlayerMovement.cs
@@ -1,25 +1,46 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Numerics;
+using Unity.Mathematics;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    public float turnspeed = 20;
     private Vector3 m_Movement;
+    private Animator m_Animator;
+    private Rigidbody m_Rigidbody;
+    Quaternion m_Rotation = Quaternion.Identity;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        m_Animator = GetComponent<Animator>();
+        m_Rigidbody = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
-    void Update()
+    void Fixed()
     {
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
 
         m_Movement.Set(horizontal, 0f, vertical);
         m_Movement.Normalize();
+
+        bool hasHorizontalInput = !Mathf.Approximately (horizontal, 0f);
+        bool hasVerticalInput = !Mathf.Approximately (vertical, 0f);
+        bool isWalking = hasHorizontalInput || hasVerticalInput;
+        m_Animator.SetBool ("IsWalking", isWalking);
+
+        Vector3 desiredForward = Vector3.RotateTowards (transform.forward, m_Movement, turnspeed * Time.deltaTime, 0f);
+        m_Rotation = Quaternion.LookRotation
+
+    }
+    
+    void OnAnimatorMove () 
+    {
+      m_Rigidbody
 
     }
 }
